dc6f2fa09b5788b501c602c8d47149f22ab0d28f
YPNM <games.art02@mail.ru>
2023-09-06 23:39:18 +0600

    fixing bugs

--- static/js/game-selector.js
+++ static/js/game-selector.js
@@ -32,7 +32,7 @@ function updateQueryStringParameter(uri, key, value) {
     const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
     const separator = uri.indexOf("?") !== -1 ? "&" : "?";
     if (uri.match(re)) {
-        return uri.replace(re, "$1" + key + "=" + value + "$2");
+        return uri.replace(re, "$1" + key + "=" + encodeURIComponent(value) + "$2");
     } else {
         return uri + separator + key + "=" + value;
     }
