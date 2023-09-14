1d1bc7ea6079329ac4cc5baa805a606a06dac526
YPNM <games.art02@mail.ru>
2023-09-14 22:01:15 +0600

    fixing bugs

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# DB_HOST = 'localhost'
-# DB_USER = 'root'
-# DB_PASS = 'QQOdmen'
-# DB_DATABASE = 'melomanquiz'
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PASS = 'QQOdmen'
+DB_DATABASE = 'melomanquiz'
 
 #
-DB_HOST='185.170.213.83'
-DB_USER='melomanquizuser'
-DB_PASS='{MeL0mAnQuIz258369}'
-DB_DATABASE='melomandb'
+# DB_HOST='185.170.213.83'
+# DB_USER='melomanquizuser'
+# DB_PASS='{MeL0mAnQuIz258369}'
+# DB_DATABASE='melomandb'
